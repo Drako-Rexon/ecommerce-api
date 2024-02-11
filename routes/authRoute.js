a661070200dad84977dc382595c52709b5073e44
Drako <abhishek491981@gmail.com>
2024-02-12 01:42:36 +0530

    sequence corrected

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -27,9 +27,13 @@ const {
     updateOrderStatus,
 } = require('../controller/userCtrl');
 
-router.put('/reset-password/:token', resetPassword);
+router.get('/all-users', getAllUsers);
 router.get('/get-orders', authMiddleware, getOrders);
-router.put('/orders/update-order/:id', authMiddleware, isAdmin, updateOrderStatus);
+router.get('/wishlist', authMiddleware, getWishlist);
+router.get('/cart', authMiddleware, getUserCart);
+router.get('/:id', authMiddleware, isAdmin, getOnlyUser);
+router.get('/refresh', handleRefreshToken);
+router.get('/logout', logout);
 router.post('/register', createUser);
 router.post('/forgot-password-token', forgotPasswordToken);
 router.post('/login', loginUserCtrl);
@@ -37,18 +41,14 @@ router.post('/admin-login', loginAdminCtrl);
 router.post('/cart', authMiddleware, userCart);
 router.post('/cart/applyCoupon', authMiddleware, applyCoupon);
 router.post('/cart/create-order', authMiddleware, createOrder);
+router.put('/orders/update-order/:id', authMiddleware, isAdmin, updateOrderStatus);
 router.put('/password', authMiddleware, updatePassword);
 router.put('/edit-user', authMiddleware, updateUser);
 router.put('/save-address', authMiddleware, saveAddress);
 router.put('/block-user/:id', authMiddleware, isAdmin, blockUser);
 router.put('/unblock-user/:id', authMiddleware, isAdmin, unblockUser);
+router.put('/reset-password/:token', resetPassword);
 router.delete('/empty-cart', authMiddleware, emptyCart);
 router.delete('/:id', deleteAUser);
-router.get('/wishlist', authMiddleware, getWishlist);
-router.get('/cart', authMiddleware, getUserCart);
-router.get('/:id', authMiddleware, isAdmin, getOnlyUser);
-router.get('/all-users', getAllUsers);
-router.get('/refresh', handleRefreshToken);
-router.get('/logout', logout);
 
 module.exports = router;
